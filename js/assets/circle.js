548a4aae2b6c124de21a64082e182023108f5504
BELLUCCI Simon <s.bellucci57@gmail.com>
2018-12-07 17:23:22 +0100

    Adds fabric.js to the project

---
+++ js/assets/circle.js
@@ -0,0 +1,25 @@
+circleButton.addEventListener('click', () => {
+
+
+  const circle = new fabric.Circle({
+    radius: 100,
+    fill: color,
+    left: canvas.width/2,
+    top: canvas.height/2,
+    id: count
+  });
+
+  canvas.add(circle);
+  count++
+
+  circle.on('moving', function() {
+    newTop = circle.top;
+    newLeft = circle.left;
+    socket.emit('newCoords', {top: newTop, left: newLeft, id: circle.id});
+  });
+
+  //Serialization
+  let serializedCanvas = JSON.stringify(canvas);
+  socket.emit('connectionCanvas', serializedCanvas);
+
+});
