6e52e226e5bc886d1d768eedfa5c0c20ff076742
BELLUCCI Simon <s.bellucci57@gmail.com>
2019-02-08 09:27:49 +0100

    Connection, register and session done

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const http = require('http').Server(app);
 
 const io = require('socket.io')(http);
 
-var knex = require('knex')({
+const session = require('express-session');
+
+const knex = require('knex')({
   client: 'mysql',
   connection: {
     host : 'base.iha.unistra.fr',
@@ -16,36 +18,28 @@ var knex = require('knex')({
   pool: { min: 0, max: 10 }
 });
 
-let crypto = require('crypto'),
-    shasum = crypto.createHash('sha1');
+const crypto = require('crypto')
+
+function sha1(data){
+  return crypto.createHash("sha1").update(data).digest("hex");
+}
+
+//Bloquer l'acces au pages si pas de session
+// app.use(function(req, res, next) {
+//     if(!req.session) {
+//         res.redirect('/connexion');
+//     } else {
+//         next();
+//     }
+// });
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-// // index page
-// app.get('/', function(req, res) {
-//   res.render('pages/index');
-// });
-
-// // about page
-// app.get('/about', function(req, res) {
-//   res.render('pages/about',
-// });
-
-//Requests
-//Récupérer le title d'un projet
-let title = ""
-
-// knex('projects').where({id: 1}).select('title').then((response)=>{
-//   console.log(response);
-//   title = response[0].title
-// });
-
 // créé le chemin vers les images
 app.use(express.static('public'));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/mincss', express.static(__dirname + '/mincss'))
 app.use('/minjs', express.static(__dirname + '/minjs'))
 
@@ -53,87 +47,148 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
+const useSession = app.use(session({
+  key: 'user_id',
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { expires: 600000 },
+}));
+
+const sessionChecker = (req, res, next) => {
+    if (req.session.user) {
+        res.redirect('/mes-projets');
+    } else {
+        next();
+    }
+};
+
+// app.use(function(req, res, next) {
+//     console.log(req.session.authenticated)
+//     if(!req.session) {
+//         res.redirect('/connexion');
+//     } else {
+//         next();
+//     }
+// });
+
 app.get('/app', function(req, res) {
-  res.render('pages/app');
+  if(req.session.user){
+    res.render('pages/app');
+  }
+	else{
+    res.redirect('/connexion');
+  }
+});
+
+app.get('/deconnexion', function(req, res){
+  req.session.user = null;
+  res.redirect('/connexion');
 });
 
 app.get('/mes-projets', function(req, res) {
-  let userId = 1;
+
   let projects = [];
   // knex.table('projects').innerJoin('users', 'users.id', '=', 'projects.author_id').where("users.id", userId).then((response) => {
   //
   // });
-  knex.table('projects').innerJoin('projects_users', 'projects.id', '=', 'projects_users.project_id').where("projects_users.user_id", userId).then((response) => {
-    response.forEach(project => {
-      projects.push(project);
-    })
-    knex.table('projects').innerJoin('users', 'users.id', '=', 'projects.author_id').where("users.id", userId).then((response) => {
+
+  if(req.session.user){
+    let userId = req.session.user[0].id;
+
+    knex.table('projects').innerJoin('projects_users', 'projects.id', '=', 'projects_users.project_id').where("projects_users.user_id", userId).then((response) => {
       response.forEach(project => {
-        // knex.table('users').innerJoin('projects_users', 'users.id', '=', 'projects_users.user_id').where("projects_users.project_id", project.id).then((response) => {
-        //   project.users = response
-        // })
         projects.push(project);
       })
-      console.log(projects);
-      res.render('pages/my-projects', {
-        infos: projects
+      knex.table('projects').innerJoin('users', 'users.id', '=', 'projects.author_id').where("users.id", userId).then((response) => {
+        response.forEach(project => {
+          projects.push(project);
+        });
+        // knex.table('users').innerJoin('projects_users', 'users.id', '=', 'projects_users.user_id').where("projects_users.project_id", 2).then((response) => {
+        //   response.forEach(project => {
+        //     projects.push(project);
+        //   });
+        // });
+        console.log(projects);
+        res.render('pages/my-projects', {
+          infos: projects
+        });
       });
-    });
-
-
-  });
-
-
-
-
+  	});
+  }else{
+    res.redirect('/connexion');
+  }
 });
 
-app.get('/connexion', function(req, res) {
+app.get('/connexion', sessionChecker, function(req, res) {
   res.render('pages/connexion');
+});
+
+app.post('/connexion', sessionChecker, function (req, res, next) {
+      //Vérification email
+      knex('users').where({ email: req.body.email }).then( response => {
+        if(response.length == 0){
+          console.log('Email inconnu');
+          res.redirect('/connexion');
+        }
+        else{
+          console.log('Email connu');
+          //Vérification correspondance user / password
+          knex('users').where({ email: req.body.email, password: sha1(req.body.password) }).then( response => {
+            if(response.length == 0){
+              console.log('Mauvais MDP');
+              res.redirect('/connexion');
+            }
+            else{
+              console.log('Bon MDP');
+              req.session.user = response;
+              res.redirect('/mes-projets')
+            }
+          });
+        }
+      });
+
 });
 
 app.get('/inscription', function(req, res) {
   res.render('pages/register');
 });
 
+app.post('/inscription', function(req, res){
+  //Vérification si l'utilisateur existe déjà : email + pseudo
+  knex('users').where({ email: req.body.email }).then( response => {
+    if(response.length == 0){
+        console.log('Pas de mail correspondant')
+
+        knex('users').where({ nickname: req.body.nickname }).then( response => {
+          if(response.length == 0){
+            console.log('Surnom non existant');
+
+            //Insertion dans la base
+            knex('users').insert({
+              lastname: req.body.lastname,
+              firstname: req.body.firstname,
+              nickname: req.body.nickname,
+              email: req.body.email,
+              password: sha1(req.body.password)
+            }).then(response => {
+              res.redirect('/connexion')
+            });
+          }
+          else{
+            console.log('Surnom existant');
+          }
+        });
+    }
+    else{
+      console.log('Un mail correspondant')
+    }
+  });
+});
+
 app.get('/canvas', function(req, res) {
   res.render('pages/canvas');
 });
-
-// server.on('request', (request, response) => {
-//   let url = request.url;
-
-//   if(url == '/'){
-//     fs.readFile('canvas.html', (error, contents) => {
-//       if(error){
-//         response.writeHead(500);
-//         response.end()
-//         return;
-//       }
-//       else{
-//         response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-//         response.write(contents);
-//         response.end();
-//       }
-//     });
-//     return;
-//   }
-//   else if (url == '/js/fabric.min.js') {
-//     fs.readFile('./js/fabric.min.js', (error, contents) => {
-//       if(error){
-//         response.writeHead(500);
-//         response.end()
-//         return;
-//       }
-//       else{
-//         response.writeHead(200, {'Content-Type': 'application/javascript; charset=utf-8'});
-//         response.write(contents);
-//         response.end();
-//       }
-//     });
-//     return;
-//   }
-// });
 
 let canvasData = '';
 let idCount = 0;
@@ -164,75 +219,6 @@ io.on('connection', (socket) => {
   socket.on('pathAdded', () => {
     idCount++
   })
-
-  //Inscription
-  socket.on('registerUser', data => {
-
-    //Hashage MDP
-    shasum.update(data.password);
-
-    //Vérification si l'utilisateur existe déjà : email + pseudo
-    knex('users').where({ email: data.email }).then( response => {
-      if(response.length == 0){
-          console.log('Pas de mail correspondant')
-
-          knex('users').where({ nickname: data.nickname }).then( response => {
-            console.log(response);
-            if(response.length == 0){
-              console.log('Surnom non existant');
-
-              //Insertion dans la base
-              knex('users').insert({
-                lastname: data.lastname,
-                firstname: data.firstname,
-                nickname: data.nickname,
-                email: data.email,
-                password: shasum.digest('hex')
-              }).then(response => {
-                console.log(response);
-              });
-            }
-            else{
-              console.log('Surnom existant');
-              socket.emit('nickNameAlreadyExists');
-            }
-          });
-      }
-      else{
-        console.log('Un mail correspondant')
-        socket.emit('emailAlreadyExists');
-      }
-    });
-  });
-
-  //Connexion
-  socket.on('connectionUser', data => {
-
-    //Hashage MDP
-    shasum.update(data.password);
-
-    //Vérification email
-    knex('users').where({ email: data.email }).then( response => {
-      if(response.length == 0){
-        console.log('Email inconnu');
-        socket.emit('emailUnknown');
-      }
-      else{
-        console.log('Email connu');
-        //Vérification correspondance user / password
-        knex('users').where({ email: data.email, password: shasum.digest('hex') }).then( response => {
-          if(response.length == 0){
-            console.log('Mauvais MDP');
-            socket.emit('wrongPassword');
-          }
-          else{
-            console.log('Bon MDP');
-          }
-        });
-      }
-    });
-
-  });
 
 });
 
